69ceab553c386baa705f4369e1e86255a5aa9b80
wangzhitao <1542344316@qq.com>
2024-08-16 13:39:27 +0800

    代码生成器学习-搭建项目结构

---
+++ easyJavaDemo/src/main/java/top/wangzhitao/RunSpringBootApplication.java
@@ -0,0 +1,14 @@
+package top.wangzhitao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 王志涛 2024/8/16
+ */
+@SpringBootApplication
+public class RunSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RunSpringBootApplication.class, args);
+    }
+}
